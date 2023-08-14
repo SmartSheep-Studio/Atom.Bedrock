95e0bdc03fb7399e49f66165eecb5736a5a4f677
LittleSheep <littlesheep.code@hotmail.com>
2023-08-15 00:42:17 +0800

    :bug: Fix well known information

--- pkg/server/hypertext/controllers/well_known.go
+++ pkg/server/hypertext/controllers/well_known.go
@@ -27,9 +27,8 @@ func (ctrl *WellKnownController) Map(router *fiber.App) {
 func (ctrl *WellKnownController) oauth(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{
 		"issuer":                   viper.GetString("general.base_url"),
-		"authorization_endpoint":   fmt.Sprintf("%s/users/openid/connect", viper.GetString("base_url")),
-		"token_endpoint":           fmt.Sprintf("%s/api/users/openid/exchange", viper.GetString("base_url")),
-		"end_session_endpoint":     fmt.Sprintf("%s/api/users/sign-out", viper.GetString("base_url")),
+		"authorization_endpoint":   fmt.Sprintf("%s/auth/openid/connect", viper.GetString("base_url")),
+		"token_endpoint":           fmt.Sprintf("%s/api/auth/openid/exchange", viper.GetString("base_url")),
 		"response_types_supported": []string{"code", "token"},
 		"grant_types_supported":    []string{"authorization_code", "implicit", "refresh_token", "password"},
 		"subject_types_supported":  []string{"public"},
@@ -56,10 +55,9 @@ func (ctrl *WellKnownController) oauth(c *fiber.Ctx) error {
 func (ctrl *WellKnownController) openid(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{
 		"issuer":                   viper.GetString("general.base_url"),
-		"authorization_endpoint":   fmt.Sprintf("%s/users/openid/connect", viper.GetString("base_url")),
-		"token_endpoint":           fmt.Sprintf("%s/api/users/openid/exchange", viper.GetString("base_url")),
-		"userinfo_endpoint":        fmt.Sprintf("%s/api/users", viper.GetString("base_url")),
-		"end_session_endpoint":     fmt.Sprintf("%s/api/users/sign-out", viper.GetString("base_url")),
+		"authorization_endpoint":   fmt.Sprintf("%s/auth/openid/connect", viper.GetString("base_url")),
+		"token_endpoint":           fmt.Sprintf("%s/api/auth/openid/exchange", viper.GetString("base_url")),
+		"userinfo_endpoint":        fmt.Sprintf("%s/api/users/self", viper.GetString("base_url")),
 		"response_types_supported": []string{"code", "token"},
 		"grant_types_supported":    []string{"authorization_code", "implicit", "refresh_token", "password"},
 		"subject_types_supported":  []string{"public"},
